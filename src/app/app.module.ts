fffa602441bb0900a1a6e2485e6561edebf113fe
Pedro <=>
2021-07-21 18:10:25 -0300

    primeiro commit do projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from "@angular/common/http";
+import { RouterModule,Routes } from "@angular/router";
+import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from './app.component';
+import { ListExpensesComponent } from './components/list-expenses/list-expenses.component';
+import { AddExpenseComponent } from './components/add-expense/add-expense.component';
+import { from } from 'rxjs';
+
+const routers: Routes = [
+  {path:'expenses', component: ListExpensesComponent},
+  {path:'addexpenses', component: AddExpenseComponent},
+  {path:'editexpense/:id', component: AddExpenseComponent},
+  {path:'', redirectTo: 'expenses',pathMatch: 'full'},
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListExpensesComponent,
+    AddExpenseComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot(routers)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/services/expense.service.ts
@@ -0,0 +1,36 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Expense } from '../models/expense';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ExpenseService {
+  private getUrl: string = "http://localhost:8181/api/v1/expenses";
+
+  constructor(private _httpClient: HttpClient) {}
+
+  getExpenses():Observable<Expense[]>{
+    return this._httpClient.get<Expense[]>(this.getUrl).pipe(
+      map(response => response)
+    )
+  }
+
+  saveExpense(expense: Expense):Observable<Expense>{
+    return this._httpClient.post<Expense>(this.getUrl,expense);
+    
+  }
+
+  getExpense(id: number):Observable<Expense>{
+    return this._httpClient.get<Expense>(`${this.getUrl}/${id}`).pipe(
+      map(response => response)
+    )
+  }
+
+  deleteExpense(id:number):Observable<any>{
+    return this._httpClient.delete(`${this.getUrl}/${id}`,{responseType:'text'});
+  }
+
+}
